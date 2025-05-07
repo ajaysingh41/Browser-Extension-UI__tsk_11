0a895494cb296f9e343c1e93d26b5165304ab063
ajaysingh41 <bannaajaysinghpanwar@gmail.com>
2025-05-07 17:04:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+let btn=document.querySelector(".theme");
+let container=document.querySelector("body");
+
+function switchMode() {
+    container.classList.toggle("dark");
+    if(container.classList.contains("dark")){
+        btn.src="images/icon-sun.svg";
+        btn.alt="light";
+        btn.classList.add("dark");
+    } else{
+        btn.src="images/icon-moon.svg";
+        btn.alt="dark";
+        btn.classList.add("light");
+    }
+    
+}
+btn.addEventListener("click",switchMode);
+
+let togglebtn=document.querySelectorAll(".toggle");
+
+togglebtn.forEach(element => {
+    element.addEventListener("click", ()=>{
+        element.classList.toggle("toggleon")
+    } );
+});
+
+let removeCardBtn=document.querySelectorAll(".remove");
+
+removeCardBtn.forEach(element=>{
+    element.addEventListener("click",()=>{
+        element.parentElement.parentElement.remove();
+    })
+})
+
+function unhide() {
+    document.querySelectorAll(".card").forEach(element=>{
+        element.style.display="flex";
+    })
+}
+
+let activeFilter=document.querySelector(".active");
+
+activeFilter.addEventListener("click",()=>{
+    unhide();
+    togglebtn.forEach(element=>{
+        if(!element.classList.contains("toggleon")){
+            element.closest(".card").style.display="none";
+        }
+        activeFilter.classList.add("filter-selected");
+        allFilter.classList.remove("filter-selected");
+        inactiveFilter.classList.remove("filter-selected");
+    })
+})
+
+let allFilter=document.querySelector(".all");
+
+allFilter.addEventListener("click",()=>{
+    unhide();
+    allFilter.classList.add("filter-selected");
+    activeFilter.classList.remove("filter-selected");
+    inactiveFilter.classList.remove("filter-selected");
+});
+
+let inactiveFilter=document.querySelector(".inactive");
+
+inactiveFilter.addEventListener("click",()=>{
+    unhide();
+    togglebtn.forEach(element=>{
+        if(element.classList.contains("toggleon")){
+            element.closest(".card").style.display="none";
+        }
+        inactiveFilter.classList.add("filter-selected");
+        allFilter.classList.remove("filter-selected");
+        activeFilter.classList.remove("filter-selected");
+    })
+});
